4be9d306b4cd0762f3bb4ca898e68bce30dea60c
BooKyuPil <71952165+Qfeel-Dev@users.noreply.github.com>
2026-01-13 16:14:52 +0900

    Add a Fastech Lib
    Add a Fastech Lib

---
+++ LIB/FASTECH/Ezi-MOTION Plus-R V6/Include_Python_x64/COMM_Define.py
@@ -0,0 +1,17 @@
+#####################################################
+# Version: 1.0.0                                    #
+# Last modified date: 25.10.31                      #
+# Changes log: First released version               #
+#####################################################
+
+
+# -*- coding: utf-8 -*-
+# Constants
+
+MAX_SLAVE_NUMS = 16
+
+MAX_BUFFER_SIZE = 256  # Maximum Send/Receive Packet Size
+MAX_SWINFO_LENGTH = 248
+
+COMM_WAITTIME = 100
+
